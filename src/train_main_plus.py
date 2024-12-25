032a9f1aa7f93a9ea668e689e7c14f61fa5d50a8
A_John <59439017+horizonjohn@users.noreply.github.com>
2024-12-25 15:39:47 +0800

    update

--- src/train_main_plus.py
+++ src/train_main_plus.py
@@ -8,9 +8,7 @@ from torch.cuda.amp import GradScaler
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from train_utils import get_acc, cross_loss
-from data_loader import LoadMyDataset
-from ViT_backbone import EncoderViT, EncoderSViT
+from train_plus_utils import EncoderViT, get_acc, cross_loss, LoadMyDataset
 from CNN_backbone import Backbone_VGG16, Backbone_Resnet50, Backbone_Inception
 
 
@@ -18,17 +16,17 @@ def train_model(args):
     if args.dataset == 'ClothesV1':
         image_path_train = './datasets/ClothesV1/trainB/'
         sketch_path_train = './datasets/ClothesV1/trainA/'
-        save_path = './checkpoint/ClothesV1/'
+        save_path = './checkpoint/ClothesV1_plus/'
 
     elif args.dataset == 'ChairV2':
         image_path_train = './datasets/ChairV2/trainB/'
         sketch_path_train = './datasets/ChairV2/trainA/'
-        save_path = './checkpoint/ChairV2/'
+        save_path = './checkpoint/ChairV2_plus/'
 
     elif args.dataset == 'ShoeV2':
         image_path_train = './datasets/ShoeV2/trainB/'
         sketch_path_train = './datasets/ShoeV2/trainA/'
-        save_path = './checkpoint/ShoeV2/'
+        save_path = './checkpoint/ShoeV2_plus/'
 
     else:
         raise ValueError('Dataset Name Error !')
@@ -56,13 +54,13 @@ def train_model(args):
     print('Dataset: {}  |  Batch size: {}\n'
           .format(args.dataset, args.batch_size))
 
-    img_model = EncoderViT(num_classes=args.num_classes, feature_dim=args.feature_dim,
-                           encoder_backbone='vit_base_patch16_224')
-    skt_model = EncoderViT(num_classes=args.num_classes, feature_dim=args.feature_dim,
-                           encoder_backbone='vit_base_patch16_224')
+    # img_model = EncoderViT(num_classes=args.num_classes, feature_dim=args.feature_dim,
+    #                        encoder_backbone='vit_base_patch16_224')
+    # skt_model = EncoderViT(num_classes=args.num_classes, feature_dim=args.feature_dim,
+    #                        encoder_backbone='vit_base_patch16_224')
 
-    # img_model = EncoderSViT(num_classes=args.num_classes, encoder_backbone='swin_base_patch4_window7_224')
-    # skt_model = EncoderSViT(num_classes=args.num_classes, encoder_backbone='swin_base_patch4_window7_224')
+    img_model = EncoderViT(num_classes=args.num_classes, device=args.device)
+    skt_model = EncoderViT(num_classes=args.num_classes, device=args.device)
 
     # img_model = Backbone_VGG16()
     # skt_model = Backbone_VGG16()
@@ -92,6 +90,7 @@ def train_model(args):
         epoch_cross_loss_anchor = 0
         epoch_cross_loss_self = 0
         epoch_cross_loss_triple = 0
+        epoch_cross_loss_decor = 0
 
         img_model.train()
         skt_model.train()
@@ -105,11 +104,12 @@ def train_model(args):
             optimizer.zero_grad()
 
             # 1.1.1 main contrastive loss
-            skt_mlp_feat, skt_vit_feat = skt_model(skt_anchor)
-            img_mlp_feat, img_vit_feat = img_model(img_anchor)
+            skt_mlp_feat, skt_vit_feat, skt_decorrelation_loss = skt_model(skt_anchor, trainable=True)
+            img_mlp_feat, img_vit_feat, img_decorrelation_loss = img_model(img_anchor, trainable=True)
 
             cross_loss_1 = cross_loss(skt_mlp_feat, img_mlp_feat, args)
 
+            ###################### ViT #######################
             # 1.1.2 self loss
             skt_aug_feat = skt_model.embedding(skt_aug)[:, 0]
             img_aug_feat = img_model.embedding(img_aug)[:, 0]
@@ -119,8 +119,23 @@ def train_model(args):
 
             # # 1.1.3 contrastive loss
             cross_loss_4 = cross_loss(skt_vit_feat[:, 0], img_vit_feat[:, 0], args)
+            ###################### ViT #######################
 
-            loss = cross_loss_1 + (cross_loss_2 + cross_loss_3) + cross_loss_4
+            # ####################### Swin #######################
+            # # 1.1.2 self loss
+            # skt_aug_feat = skt_model.embedding(skt_aug)
+            # img_aug_feat = img_model.embedding(img_aug)
+            #
+            # cross_loss_2 = cross_loss(skt_aug_feat, skt_vit_feat, args)
+            # cross_loss_3 = cross_loss(img_aug_feat, img_vit_feat, args)
+            #
+            # # # 1.1.3 contrastive loss
+            # cross_loss_4 = cross_loss(skt_vit_feat, img_vit_feat, args)
+            # ####################### Swin #######################
+
+            cross_loss_5 = skt_decorrelation_loss + img_decorrelation_loss
+
+            loss = cross_loss_1 + (cross_loss_2 + cross_loss_3) + cross_loss_4 + cross_loss_5
 
             scaler.scale(loss).backward()
             scaler.step(optimizer)
@@ -130,12 +145,14 @@ def train_model(args):
             epoch_cross_loss_anchor = epoch_cross_loss_anchor + cross_loss_1.item()
             epoch_cross_loss_self = epoch_cross_loss_self + (cross_loss_2 + cross_loss_3).item()
             epoch_cross_loss_triple = epoch_cross_loss_triple + cross_loss_4.item()
+            epoch_cross_loss_decor = epoch_cross_loss_decor + cross_loss_5.item()
 
         print('Epoch Train: [{}] Contrastive Loss: {}'.format(epoch, epoch_train_contrastive_loss))
         wandb.log({'Contrastive Loss': epoch_train_contrastive_loss}, step=epoch)
         wandb.log({'Cross Loss Anchor': epoch_cross_loss_anchor}, step=epoch)
         wandb.log({'Self Loss': epoch_cross_loss_self}, step=epoch)
         wandb.log({'Triple Loss': epoch_cross_loss_triple}, step=epoch)
+        wandb.log({'Decor Loss': epoch_cross_loss_decor}, step=epoch)
 
         img_model.eval()
         skt_model.eval()
@@ -164,10 +181,7 @@ def train_model(args):
             wandb.log({'Top10 Acc Train': top10_acc_train}, step=epoch)
 
         # 1.3 save checkpoints
-        if (top1_accuracy > args.best_top1_acc) or \
-                (top1_accuracy == args.best_top1_acc and top10_accuracy > args.best_top10_acc) or \
-                (top1_accuracy == args.best_top1_acc and top10_accuracy == args.best_top10_acc and
-                 top5_accuracy > args.best_top5_acc):
+        if top1_accuracy > args.best_top1_acc:
             args.best_top1_acc = top1_accuracy
             args.best_top5_acc = top5_accuracy
             args.best_top10_acc = top10_accuracy
@@ -178,19 +192,34 @@ def train_model(args):
                           'top1': top1_accuracy,
                           'top5': top5_accuracy,
                           'top10': top10_accuracy}
-            print('Updating Network checkpoint [Best Acc]...')
+            print('Updating Model checkpoint [Best Acc]...')
             torch.save(save_state, os.path.join(save_path, 'model_Best.pth'))
 
-        if epoch % args.save_iter == 0:
-            save_state = {'img_model': img_model.state_dict(),
-                          'skt_model': skt_model.state_dict(),
-                          'epoch': epoch,
-                          'loss': round(epoch_train_contrastive_loss, 5),
-                          'top1': top1_accuracy,
-                          'top5': top5_accuracy,
-                          'top10': top10_accuracy}
-            print('Updating Network checkpoint...')
-            torch.save(save_state, os.path.join(save_path, 'model_' + str(epoch) + '.pth'))
+        if top1_accuracy == args.best_top1_acc:
+            if top5_accuracy > args.best_top5_acc:
+                args.best_top1_acc = top1_accuracy
+                args.best_top5_acc = top5_accuracy
+                save_state = {'img_model': img_model.state_dict(),
+                              'skt_model': skt_model.state_dict(),
+                              'epoch': epoch,
+                              'loss': round(epoch_train_contrastive_loss, 5),
+                              'top1': top1_accuracy,
+                              'top5': top5_accuracy,
+                              'top10': top10_accuracy}
+                print('Updating Network checkpoint...')
+                torch.save(save_state, os.path.join(save_path, 'model_' + str(epoch) + '.pth'))
+            elif top10_accuracy > args.best_top10_acc:
+                args.best_top1_acc = top1_accuracy
+                args.best_top10_acc = top10_accuracy
+                save_state = {'img_model': img_model.state_dict(),
+                              'skt_model': skt_model.state_dict(),
+                              'epoch': epoch,
+                              'loss': round(epoch_train_contrastive_loss, 5),
+                              'top1': top1_accuracy,
+                              'top5': top5_accuracy,
+                              'top10': top10_accuracy}
+                print('Updating Network checkpoint...')
+                torch.save(save_state, os.path.join(save_path, 'model_' + str(epoch) + '.pth'))
 
     print('Best Acc:\nTop1: {:.3f} %  |  Top5: {:.3f} %  |  Top10: {:.3f} %'.format(args.best_top1_acc,
                                                                                     args.best_top5_acc,
